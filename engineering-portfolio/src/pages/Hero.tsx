f96247fe50635d11a2b876027e943a34fb88795e
Otonbara <140208396+Otonbara@users.noreply.github.com>
2025-10-23 18:04:02 +0100

    Add framer-motion and motion dependencies; update Hero component layout

--- engineering-portfolio/src/pages/Hero.tsx
+++ engineering-portfolio/src/pages/Hero.tsx
@@ -37,14 +37,14 @@ export default function Hero () {
             </div>
             <div className="relative flex flex-col-reverse md:flex-row items-center justify-evenly gap-10 px-6 pt-24 md:px-24 z-20">
                 {/* Text Section */}
-                <div className="flex flex-col justify-center items-start text-left space-y-4">
+                <div className="flex flex-col justify-center items-center md:items-start space-y-4">
                     <h1 className="text-3xl md:text-4xl font-bold bg-linear-to-r from-accent
                         via-purple-400 to-blue-500 bg-clip-text text-transparent
                         inline-block overflow-hidden whitespace-nowrap pr-1
-                        animate-typing">
+                        animate-typing text-center">
                         OTONBARA ALFRED OKOLAI
                     </h1>
-                    <h2 className="text-lg md:text-xl text-text-secondary">CIVIL & ENVIRONMENTAL ENGINEER</h2>
+                    <h2 className="text-lg md:text-xl text-text-secondary text-center">CIVIL & ENVIRONMENTAL ENGINEER</h2>
                     <h3 className="text-sm md:text-base text-text-secondary">BASED IN LAGOS, NIGERIA</h3>
                 </div>
                 {/* Image Section */}
